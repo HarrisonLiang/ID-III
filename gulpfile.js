16eac7edf24210046f1c428e1741a3be8d676a0a
EC2 Default User <ec2-user@ip-172-31-26-213.ec2.internal>
2018-10-10 20:16:22 +0000

    Added SCSS to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass');
 
 // Scripts Tasks
 // Uglifies
 gulp.task('scripts', function(){
     gulp.src('Source/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('App/scripts'))    
+    .pipe(gulp.dest('App/scripts'))
 });
 
 //Styles Tasks
@@ -14,6 +15,13 @@ gulp.task('styles', function(){
     console.log('runs styles');
 });
 
+//SCSS Tasks
+gulp.task('sass', function(){
+    return gulp.src('Source/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('App/styles'))
+});
+
 // Default Runs Everything
-gulp.task('default', ['scripts', 'styles']);
+gulp.task('default', ['scripts', 'styles', 'sass']);
 
