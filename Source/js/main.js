927388a94ba980120f8f0a3c05400ab28210bb43
EC2 Default User <ec2-user@ip-172-31-26-213.ec2.internal>
2018-10-30 03:12:30 +0000

    first website draft

--- Source/js/main.js
+++ Source/js/main.js
@@ -1,4 +1,4 @@
-function showMiles(){
+/*function showMiles(){
     
 var totalGallons=200; //total Gallons
 var milesPerGallon=16; //miles per Gallon
@@ -6,4 +6,31 @@ var milesPerGallon=16; //miles per Gallon
 
     alert ("You have used " + totalGallons/milesPerGallon + " gallons of fuel");
     
-}
+}*/
+
+/*global $
+$(document).ready(function () {
+    $(".carousel").carousel();
+    
+    $('.carousel').carousel({
+      interval: 2000
+    })
+    
+      $(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 3100,
+        pause:null
+    });
+    
+    $('.carousel').carousel('cycle');
+    });
+    
+    $(".carousel-control-prev").click(function(){
+    $("#carouselExampleIndicators").carousel("prev")
+    });
+    
+    $(".carousel-control-next").click(function(){
+    $("#carouselExampleIndicators").carousel("prev")    
+    });
+});
+*/
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
-    sass = require('gulp-sass');
-
+    sass = require('gulp-sass'),
+    imagemin = require('gulp-imagemin');
+    
 // Scripts Tasks
 // Uglifies
 gulp.task('scripts', function(){
@@ -32,10 +33,22 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('App/styles'));
 });
 
+gulp.task('images', function() {
+    return gulp.src('Source/images/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest('App/images'))
+})
+
 gulp.task('sass:watch', function () {
   gulp.watch('Source/scss/*.scss', ['sass']);
 });
 
+gulp.task('watch', function() {
+    gulp.watch('Source/js/*.js', ['scripts']);
+    gulp.watch('Source/scss/**/*.scss', ['sass']);
+    gulp.watch('Source/images/**', ['images']);
+});
+
 // Default Runs Everything
-gulp.task('default', ['scripts', 'styles', 'sass']);
+gulp.task('default', ['scripts', 'styles', 'sass', 'images']);
 
